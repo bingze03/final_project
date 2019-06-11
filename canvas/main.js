2d22309c2fe382e61b8fb8cf8db69a761c0c839c
bingze <gooogle126@gmail.com>
2019-06-11 22:57:46 +0800

    combine all features, canvas background setup

--- canvas/main.js
+++ canvas/main.js
@@ -1,3 +1,17 @@
-$(document).ready(()=>{
+import html2canvas from 'html2canvas';
+
+var ctx;
+/*
+[todo]
+鋼鐵人和薩諾斯互相以衝擊波制衡在中間
+每次會產生5~8個上下左右組成的指令(要做上下左右的圖)
+user需在3秒內(progress bar)完成
+success則鋼鐵人的衝擊波往薩諾斯推進
+fail則薩諾斯的衝擊波往鋼鐵人推進
+*/
+$(document).ready(function(){
+    ctx=$('#myCanvas')[0].getContext('2d');
+
 
 });
+
